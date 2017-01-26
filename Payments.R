0e8905ded9c049e333d858f6f2e56f3001f500cd
Francisco Gutiérrez <frank23@Franciscos-MacBook-Pro.local>
2017-01-26 19:24:59 +0100

    Updated readme

--- Payments.R
+++ Payments.R
@@ -1,23 +1,23 @@
-data <- read.csv("output2.csv") 
+data <- read.csv("output2.csv")
 
-split.data <- split(data, data$userid) 
+split.data <- split(data, data$userid)
 
 ls.names     <- c()
-ls.questions <- c() 
-ls.correct   <- c() 
-ls.viz       <- c() 
-ls.payment   <- c() 
+ls.questions <- c()
+ls.correct   <- c()
+ls.viz       <- c()
+ls.payment   <- c()
 ls.timestamp <- c()
 #  Check users and questions
 for (user in split.data) {
-    user.subset  <- subset(user, 
+    user.subset  <- subset(user,
                            select = c("userid"  ,
                                       "question",
                                       "correct" ,
                                       "viz"))
     user.ordered       <- user.subset[order(user.subset$question),]
     # Prepare output
-    user.num.questions <- 0 
+    user.num.questions <- 0
     user.num.correct   <- 0
     user.payment       <- 0.5
     user.id            <- user$userid[1]
@@ -29,7 +29,7 @@ for (user in split.data) {
         if (user$correct[i] == "true") user.num.correct  <- user.num.correct + 1
     }
     if (user.num.correct > 7 ) user.payment <- 1.5
-    if (user.num.correct > 10) user.payment <- 3.0 
+    if (user.num.correct > 10) user.payment <- 3.0
     # output
     # checking if this is the latest study...
     if (user.timestamp > 1476000000000) {
@@ -43,8 +43,8 @@ for (user in split.data) {
 }
 
 # create a dataframe for the output
-output <- data.frame(ls.names,            
-                     ls.viz,            
+output <- data.frame(ls.names,
+                     ls.viz,
                      ls.questions,
                      ls.timestamp,
                      ls.correct,
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# StockPrices_Analysis
-My R scripts for analysing our study
+# Which Visualisation Works When.
+
+Supplementary material for research paper. (Data and code to be submitted after publication.)
